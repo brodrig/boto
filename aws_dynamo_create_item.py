9054e53fb48bbd4d254c75b19e4c47451649723a
brodrig <briancosmorodriguez@gmail.com>
2015-11-09 23:08:45 -0500

    NoSQL on AWS - second  one

---
+++ aws_dynamo_create_item.py
@@ -0,0 +1,26 @@
+
+import boto3
+
+# Get the service resource.
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('users')
+
+table.put_item(
+   Item={
+        'username': 'Jon',
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'age': 100,
+        'account_type': 'webmaster',
+        }
+)
+
+table.put_item(
+   Item={
+        'username': 'J',
+        'first_name': 'Jack',
+        'last_name': 'Smith',
+        'age': 100,
+        'account_type': 'webmaster',
+        }
+)
